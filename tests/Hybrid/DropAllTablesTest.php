8041dd8e729e0f2b566039938a76a8c88946fcda
Aaron Francis <Aarondfrancis@gmail.com>
2022-06-28 15:49:10 -0500

    Emulate prepares and add test for dropping all tables

---
+++ tests/Hybrid/DropAllTablesTest.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @author Aaron Francis <aarondfrancis@gmail.com|https://twitter.com/aarondfrancis>
+ */
+
+namespace SingleStore\Laravel\Tests\Hybrid;
+
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use SingleStore\Laravel\Schema\Blueprint;
+use SingleStore\Laravel\Tests\BaseTest;
+
+class DropAllTablesTest extends BaseTest
+{
+    use HybridTestHelpers;
+
+    /** @test */
+    public function it_drops_all_tables_sequentially()
+    {
+        if (!$this->runHybridIntegrations()) {
+            return;
+        }
+
+        $this->mockDatabaseConnection = false;
+
+        Schema::dropIfExists('test_drop_1');
+        Schema::dropIfExists('test_drop_2');
+
+        $this->assertFalse(Schema::hasTable('test_drop_1'));
+        $this->assertFalse(Schema::hasTable('test_drop_2'));
+
+        Schema::create('test_drop_1', function(Blueprint $table) {
+            $table->id();
+        });
+
+        Schema::create('test_drop_2', function (Blueprint $table) {
+            $table->id();
+        });
+
+        $this->assertTrue(Schema::hasTable('test_drop_1'));
+        $this->assertTrue(Schema::hasTable('test_drop_2'));
+
+        $this->getConnection()->getSchemaBuilder()->dropAllTables();
+    }
+
+}
