1f9bc9d612cbebc678aab6e27cc8f7589e102059
Adalbert Makarovych <amakarovych-ua@memsql.com>
2022-11-30 23:14:30 +0200

    Added more tests

--- tests/Hybrid/UnionTest.php
+++ tests/Hybrid/UnionTest.php
@@ -10,23 +10,30 @@ class UnionTest extends BaseTest
 {
     use HybridTestHelpers;
 
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        if ($this->runHybridIntegrations()) {
+            $this->createTable(function (Blueprint $table) {
+                $table->id();
+            });
+
+            DB::table('test')->insert([
+                ['id' => 1],
+                ['id' => 2],
+                ['id' => 3],
+                ['id' => 4],
+                ['id' => 100],
+            ]);
+        }
+    }
+
     /** @test */
     function union() {
         if (! $this->runHybridIntegrations()) {
             return;
         }
-
-        $this->createTable(function (Blueprint $table) {
-            $table->id();
-        });
-
-        DB::table('test')->insert([
-            ['id' => 1],
-            ['id' => 2],
-            ['id' => 3],
-            ['id' => 4],
-            ['id' => 100],
-        ]);
 
         $first = DB::table('test')->where('id', '<', 3);
         $second = DB::table('test')->where('id', '>', 5);
@@ -41,22 +48,28 @@ class UnionTest extends BaseTest
     }
 
     /** @test */
-    function unionWithOrderByLimitAndOffset() {
+    function unionAll() {
         if (! $this->runHybridIntegrations()) {
             return;
         }
 
-        $this->createTable(function (Blueprint $table) {
-            $table->id();
-        });
+        $first = DB::table('test')->where('id', '<', 4);
+        $second = DB::table('test')->where('id', '>', 2);
+        $res = $first->unionAll($second)->get();
 
-        DB::table('test')->insert([
-            ['id' => 1],
-            ['id' => 2],
-            ['id' => 3],
-            ['id' => 4],
-            ['id' => 100],
-        ]);
+        $indexes = array_map(function ($value): int {
+            return $value->id;
+        }, $res->toArray());
+        sort($indexes);
+
+        $this->assertEquals([1, 2, 3, 3, 4, 100], $indexes);
+    }
+
+    /** @test */
+    function unionWithOrderByLimitAndOffset() {
+        if (! $this->runHybridIntegrations()) {
+            return;
+        }
 
         $first = DB::table('test')->where('id', '<', 3);
         $second = DB::table('test')->where('id', '>', 5);
@@ -65,9 +78,50 @@ class UnionTest extends BaseTest
         $indexes = array_map(function ($value): int {
             return $value->id;
         }, $res->toArray());
-        sort($indexes);
 
         $this->assertEquals([2], $indexes);
     }
 
+    /** @test */
+    function unionWithOrderBy() {
+        if (! $this->runHybridIntegrations()) {
+            return;
+        }
+
+        $first = DB::table('test')->where('id', '<', 3);
+        $second = DB::table('test')->where('id', '>', 5);
+        $res = $first->union($second)->orderBy('id')->get();
+
+        $indexes = array_map(function ($value): int {
+            return $value->id;
+        }, $res->toArray());
+
+        $this->assertEquals([1, 2, 100], $indexes);
+    }
+
+    /** @test */
+    function unionWithLimit() {
+        if (! $this->runHybridIntegrations()) {
+            return;
+        }
+
+        $first = DB::table('test')->where('id', '<', 3);
+        $second = DB::table('test')->where('id', '>', 5);
+        $res = $first->union($second)->limit(2)->get();
+
+        $this->assertCount(2, $res);
+    }
+
+    /** @test */
+    function unionWithOffset() {
+        if (! $this->runHybridIntegrations()) {
+            return;
+        }
+
+        $first = DB::table('test')->where('id', '<', 3);
+        $second = DB::table('test')->where('id', '>', 5);
+        $res = $first->union($second)->offset(1)->limit(100000)->get();
+
+        $this->assertCount(2, $res);
+    }
 }
