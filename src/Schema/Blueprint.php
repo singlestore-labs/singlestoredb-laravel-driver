090b1aa63effdb76f692c30f453469cf92fc5d8f
Adalbert Makarovych <amakarovych-ua@memsql.com>
2023-02-22 10:17:34 +0200

    Added support of the Laravel 10
    Summary:
    Changed the inlining of the indexes.
    The previous implementation assumed that the number of commands equals the number of generated statements but this is not always true.

--- src/Schema/Blueprint.php
+++ src/Schema/Blueprint.php
@@ -27,13 +27,6 @@ class Blueprint extends BaseBlueprint
         return $this->point($column);
     }
 
-    public function toSql(Connection $connection, Grammar $grammar)
-    {
-        $statements = parent::toSql($connection, $grammar);
-
-        return $this->creating() ? $this->inlineCreateIndexStatements($statements) : $statements;
-    }
-
     /**
      * Execute the blueprint against the database.
      *
