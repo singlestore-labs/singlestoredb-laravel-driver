2834bce4b1b4203da46c0db8d7cc628dfaf89b77
Adalbert Makarovych <amakarovych-ua@singlestore.com>
2023-07-31 14:31:05 +0300

    Added possibility to set query options

--- src/Query/Builder.php
+++ src/Query/Builder.php
@@ -6,4 +6,21 @@ use Illuminate\Database\Query\Builder as BaseBuilder;
 
 class Builder extends BaseBuilder
 {
+    public $options;
+
+    public function options(array $options) {
+        $this->options = $options;
+        return $this;
+    }
+
+    public function toSql()
+    {
+        $sql = parent::toSql();
+
+        if (!empty($this->options)) {
+            $sql .= ' '.$this->grammar->compileOptions($this->options);
+        }
+
+        return $sql;
+    }
 }
