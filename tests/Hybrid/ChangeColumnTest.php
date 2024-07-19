12ce39bc5aa5ac2fe195e86478a498b4571ac4a2
Hafez Divandari <hafezdivandari@gmail.com>
2024-07-19 15:57:31 +0330

    force native schema operations

--- tests/Hybrid/ChangeColumnTest.php
+++ tests/Hybrid/ChangeColumnTest.php
@@ -5,6 +5,7 @@ namespace SingleStore\Laravel\Tests\Hybrid;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Schema;
 use SingleStore\Laravel\Schema\Blueprint;
+use SingleStore\Laravel\Schema\Builder;
 use SingleStore\Laravel\Tests\BaseTest;
 
 class ChangeColumnTest extends BaseTest
@@ -42,6 +43,10 @@ class ChangeColumnTest extends BaseTest
             $cached = $this->mockDatabaseConnection;
 
             $this->mockDatabaseConnection = false;
+
+            if (method_exists(Builder::class, 'useNativeSchemaOperationsIfPossible')) {
+                Schema::useNativeSchemaOperationsIfPossible();
+            }
 
             $this->createTable(function (Blueprint $table) {
                 $table->rowstore();
