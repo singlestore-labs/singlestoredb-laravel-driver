75440487049e9c863c4b9f8d4f9b45b6b01f324d
Hafez Divandari <hafezdivandari@gmail.com>
2024-07-24 10:37:59 +0330

    fix tests

--- tests/Hybrid/ChangeColumnTest.php
+++ tests/Hybrid/ChangeColumnTest.php
@@ -38,8 +38,9 @@ class ChangeColumnTest extends BaseTest
                 $table->text('data')->nullable()->change();
             });
 
+            $this->assertEquals(['id', 'data'], Schema::getColumnListing('test'));
+
             if (version_compare(Application::VERSION, '10.30', '>=')) {
-                $this->assertEquals(['id', 'data'], Schema::getColumnListing('test'));
                 $this->assertEquals('text', Schema::getColumnType('test', 'data'));
             }
 
